b2a738e3c6949a161aef32293f87882e6e7872bf
Markel Cuesta <cuestaarribas.markel@proton.me>
2025-12-21 15:55:33 +0100

    Removed unnecessary export

--- src/output/index.ts
+++ src/output/index.ts
@@ -1,6 +1,5 @@
-export { OutputView } from './OutputView.js';
-export { StdoutView } from './StdoutView.js';
-export { RedisPublisherView } from './RedisPublisherView.js';
-export type { RedisPublisherOptions } from './RedisPublisherView.js';
-export { CompositeView } from './CompositeView.js';
-export type { OutputMessage, MessageType } from './types.js';
+export { OutputView } from "./OutputView.js";
+export { StdoutView } from "./StdoutView.js";
+export { RedisPublisherView } from "./RedisPublisherView.js";
+export type { RedisPublisherOptions } from "./RedisPublisherView.js";
+export { CompositeView } from "./CompositeView.js";
